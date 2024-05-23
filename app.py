67cbe4bd31696ac47050287f2938bdc5d26035ef
Kolade <koladeatanseiye@gmail.com>
2024-05-23 12:14:20 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
